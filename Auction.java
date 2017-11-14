d7972d16778377ff0c8c77211e54586d427d5bd9
vasiliychu <kogeto3@gmail.com>
2017-11-14 21:44:38 +0400

    test

---
+++ Auction.java
@@ -0,0 +1,14 @@
+import com.view.au.UserView;
+/**
+ * In the main class, we run only the parameters of the first authorization menu
+ *
+ * @author Vasiliy
+ * @version 1.0
+ * */
+public class Auction {
+    public static void main(String[] args) {
+        UserView userView = new UserView();
+        userView.UserView();
+
+    }
+}
